b61a30f8b69a5e933c52209e496d39bfd713af05
drrt <drrt@cs.utexas.edu>
2013-08-29 00:37:38 -0500

    alias laff is now laffvm

--- configure.sh
+++ configure.sh
@@ -1,4 +1,4 @@
-echo "alias laff='cd ~/laff; vagrant up; (sleep 4 && open http://127.0.0.1:8888 &) > /dev/null 2>&1; vagrant ssh; vagrant halt -f'" >> ~/.profile
+echo "alias laffvm='cd ~/laff; vagrant up; (sleep 4 && open http://127.0.0.1:8888 &) > /dev/null 2>&1; vagrant ssh; vagrant halt -f'" >> ~/.profile
 vagrant up
 vagrant halt
 git rm ~/laff/configure.sh
