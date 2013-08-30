4b6f1a8b7bec12d56bb1c7f19b544e786b06c4f9
drrt <drrt@cs.utexas.edu>
2013-08-30 18:54:54 -0500

    fixed typo and added exit

--- unix_config.sh
+++ unix_config.sh
@@ -1,4 +1,5 @@
 echo "alias laffvm='cd ~/laff; vagrant up; (sleep 4 && open http://127.0.0.1:8888 &) > /dev/null 2>&1; vagrant ssh; vagrant halt -f'" >> ~/.profile
 vagrant up
 vagrant halt
-git rm ~/laff/configure.sh
+git rm ~/laff/unix_config.sh
+exit
