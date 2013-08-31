70c76b8247bb79462f5f5375f3070d0ebefe388e
rosatamsen <drrt@cs.utexas.edu>
2013-08-31 16:03:01 -0500

    fixed alias

--- config/win.ps1
+++ config/win.ps1
@@ -1,4 +1,7 @@
 new-item -path $profile -type file -force
-echo "set-alias laffvm 'cd ~/laff; vagrant up; start http://127.0.0.1:8888; vagrant ssh; vagrant halt -f'" >> $profile
+new-item -path $profile -type file -force
+echo "function laffvm{cd ~/laff; vagrant up; start http://127.0.0.1:8888; vagrant ssh; vagrant halt -f}" >> $profile
+echo "set-alias to laffvm" >> $profile
+vagrant up
 vagrant up
 vagrant halt
