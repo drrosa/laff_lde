87127e8b10f6ae2a00a3d77142d70b1215f69a6a
drrt <drrt@cs.utexas.edu>
2013-08-30 20:27:31 -0500

    added source ~/.profile

--- config/linux.sh
+++ config/linux.sh
@@ -1,3 +1,4 @@
 echo "alias laffvm='cd ~/laff; vagrant up; (sleep 4 && xdg-open http://127.0.0.1:8888 &) > /dev/null 2>&1; vagrant ssh; vagrant halt -f'" >> ~/.profile
+source ~/.profile
 vagrant up
 vagrant halt
